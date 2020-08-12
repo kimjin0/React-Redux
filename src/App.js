52757d43192182cb447fb12a2d6e720efa368cdc
kimjin0 <pkimjin0q@gamil.com>
2020-08-12 14:36:39 +0900

    layout 구성

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import AddNumberRoot from './component/AddNumberRoot';
+import DisplayNumberRoot from './component/DisplayNumberRoot';
+
+class App extends Component {
+    state = {
+        number: 0,
+    };
+    render() {
+        return (
+            <div className="App">
+                <h1>Root</h1>
+                <AddNumberRoot></AddNumberRoot>
+                <DisplayNumberRoot number={this.state.number}></DisplayNumberRoot>
+            </div>
+        );
+    }
+}
+
+export default App;
