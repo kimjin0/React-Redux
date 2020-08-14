fdb6b1f0925656e691b45b52c5c7a9d0f3d3dc16
kimjin0 <pkimjin0q@gamil.com>
2020-08-14 14:05:45 +0900

    마법같은 react-redux

--- src/container/AddNumber.jsx
+++ src/container/AddNumber.jsx
@@ -1,14 +1,25 @@
 import AddNumber from '../component/AddNumber';
-import React, { Component } from 'react';
-import store from '../store';
-export default class extends Component {
-    render() {
-        return (
-            <AddNumber
-                onClick={function (size) {
-                    store.dispatch({ type: 'INCREMENT', size: size });
-                }.bind(this)}
-            ></AddNumber>
-        );
-    }
+import { connect } from 'react-redux';
+
+function mapReduxDispatchToReactProps(dispatch) {
+    return {
+        onClick: function (size) {
+            dispatch({ type: 'INCREMENT', size: size });
+        },
+    };
 }
+
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
+// import React, { Component } from 'react';
+// import store from '../store';
+// export default class extends Component {
+//     render() {
+//         return (
+//             <AddNumber
+//                 onClick={function (size) {
+//                     store.dispatch({ type: 'INCREMENT', size: size });
+//                 }}
+//             ></AddNumber>
+//         );
+//     }
+// }
--- src/container/DisplayNumber.jsx
+++ src/container/DisplayNumber.jsx
@@ -1,20 +1,31 @@
 import DisplayNumber from '../component/DisplayNumber';
-import React, { Component } from 'react';
-import store from '../store';
-
-export default class extends Component {
-    state = { number: store.getState().number };
-    constructor(props) {
-        super(props);
-        store.subscribe(
-            function () {
-                this.setState({
-                    number: store.getState().number,
-                });
-            }.bind(this)
-        );
-    }
-    render() {
-        return <DisplayNumber number={this.state.number}></DisplayNumber>;
-    }
+import { connect } from 'react-redux';
+function mapReduxStateToReactProps(state) {
+    return {
+        number: state.number,
+    };
 }
+function mapReduxDispatchToReactProps() {
+    return {};
+}
+
+export default connect(mapReduxStateToReactProps, mapReduxDispatchToReactProps)(DisplayNumber);
+// import React, { Component } from 'react';
+// import store from '../store';
+
+// export default class extends Component {
+//     state = { number: store.getState().number };
+//     constructor(props) {
+//         super(props);
+//         store.subscribe(
+//             function () {
+//                 this.setState({
+//                     number: store.getState().number,
+//                 });
+//             }.bind(this)
+//         );
+//     }
+//     render() {
+//         return <DisplayNumber number={this.state.number}></DisplayNumber>;
+//     }
+// }
